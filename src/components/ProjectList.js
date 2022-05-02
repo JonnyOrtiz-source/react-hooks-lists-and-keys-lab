17429c48e05c2f6b048c82fbfb037cb062b8d5be
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-05-02 19:49:05 -0400

    Completed assignment

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,17 @@
-import React from "react";
-import ProjectItem from "./ProjectItem";
+import React from 'react';
+import ProjectItem from './ProjectItem';
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
-    </div>
-  );
+   const projectsElements = projects.map((project) => {
+      return <ProjectItem key={project.id} {...project} />;
+   });
+
+   return (
+      <div id="projects">
+         <h2>My Projects</h2>
+         <div id="project-list">{projectsElements}</div>
+      </div>
+   );
 }
 
 export default ProjectList;
